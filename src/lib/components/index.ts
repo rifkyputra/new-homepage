2d112e177a299403037e1f5cd4a278925b2f1cb4
Rifky A. Putra <rifqempul@gmail.com>
2025-10-19 18:39:33 +0800

    Revert "init"
    This reverts commit e8dc1d81e9a2d9511ae85ee03cbd402de8880ea3.

--- src/lib/components/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export components for easier imports
-export { default as CodeBlock } from "./CodeBlock.svelte";
-export { default as Nav } from "./Nav.svelte";
-export { default as LiquidGlass } from "./LiquidGlass.svelte";
-export { default as Footer } from "./Footer.svelte";
-export { default as TechStack } from "./TechStack.svelte";
-export { default as QASection } from "./QASection.svelte";
